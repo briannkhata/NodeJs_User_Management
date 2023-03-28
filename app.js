12883afdd9c991c9b069baa09b546546181e1b4a
Brian Nkhata <bnkhata@mra.mw>
2023-03-28 14:12:58 +0200

    bug fix

--- app.js
+++ app.js
@@ -1,42 +1,3 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const path = require("path");
-// const connectDB = require("./server/config/db");
-// const expressLayouts = require("express-ejs-layouts");
-// const ejs = require("ejs");
-// const customerRoutes = require("./server/routes/customer");
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// // Connect to MongoDB database
-// connectDB();
-
-// // Middleware for parsing request body
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-
-// //Templating engine
-// app.use(expressLayouts);
-// app.set("layout", "./layouts/main");
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
-
-// app.use("/", customerRoutes);
-
-// // Handle 404 errors
-// app.use((req, res) => {
-//   res.status(404).render("404", { pageTitle: "Page Not Found" });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-
-
 require("dotenv").config();
 
 const express = require("express");
@@ -51,9 +12,10 @@ const port = process.env.PORT || 5000;
 //Connect to MongoDB database
 connectDB();
 
-// Middleware for parsing request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//statuc files
 app.use(express.static("public"));
 
 // Templating engine
@@ -62,10 +24,13 @@ app.set("views", path.join(__dirname, "views"));
 app.use(expressLayouts);
 app.set('layout', path.join(__dirname, 'views/layouts/main'));
 
-//app.set("layout", "layouts/main");
-
 // Mount routes
-app.use("/", customerRoutes);
+
+app.get('/', (req, res) => {
+  res.render('index'); // assuming the file is called myfile.ejs and is located in your views folder
+});
+
+//app.use("/", customerRoutes);
 
 // Handle 404 errors
 app.use((req, res) => {
